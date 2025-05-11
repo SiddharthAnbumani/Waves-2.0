88a50802a40d3d9849ccb375afe0c3f2c5014375
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-05-11 19:11:28 +0530

    NI

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -2,7 +2,7 @@ export default function Canvas(){
   return (
    <div className="h-screen w-screen justify-center items-center">
     <p className="text-9xl font-extrabold" >
-      This Cavas will be the main designing platform for Components
+
     </p>
    </div>
 
