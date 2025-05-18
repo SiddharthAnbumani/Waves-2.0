81acee37bf833beacd351ed603d468810495d54b
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-05-18 17:07:50 +0530

    NI

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function Canvas() {
   return (
    <div className="">
-    Have to focus on the next Component - Vetical Banner and see if i can use the horizontal banner in the Ph devices using conditional rendering
+
    </div>
   );
 }
