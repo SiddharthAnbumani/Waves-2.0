ca60629a9d8ea3dcd878807acf2ca6c6427456e3
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-05-25 09:44:49 +0530

    NI

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function Canvas() {
   return (
    <div className="">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quam voluptatibus adipisci expedita molestiae! Necessitatibus, dolorem delectus voluptates et sequi dignissimos cumque autem minima ipsum, veritatis esse nisi blanditiis corporis.
+
    </div>
   );
 }
